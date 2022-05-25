6c0f9b6e72cdb9c260b582ace0e38b72bcfeb0f4
RellzCodez <adeyemiodunayo21@gmail.com>
2022-05-25 05:18:54 -0700

    added components and worked on it

---
+++ portfolio/src/App.js
@@ -0,0 +1,19 @@
+
+import './App.css';
+import Body from './components/Body';
+import Open from './components/Open';
+
+
+function App() {
+  return (
+    <div>
+     
+    <Open />
+    <Body />
+      
+
+    </div>
+  );
+}
+
+export default App;
---
+++ portfolio/src/components/Open.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import './Open.css'
+
+const Open = () => {
+  return (
+    <div className='App'>
+        <h1 className='text' > Welcome to my portfolio </h1>
+        <h1> I AM ODUNAYO ADEYEMI <br />  I'm a Front-end Web Developer, Writer and Mentor.</h1>
+
+    </div>
+  )
+}
+
+export default Open
