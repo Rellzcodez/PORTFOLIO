a2c2eb89a43c6a4599e29641476e0eb5d7130e0d
RellzCodez <adeyemiodunayo21@gmail.com>
2022-05-30 09:02:31 -0700

    created the home page and modified

--- portfolio/src/App.js
+++ portfolio/src/App.js
@@ -1,16 +1,17 @@
 
 import './App.css';
-import Body from './components/Body';
-import Open from './components/Open';
+/*import Body from './components/Body'; */
+/*import Open from './components/Open'; */
+import Home from './Home';
 
 
 function App() {
   return (
     <div>
-     
-    <Open />
-    <Body />
-    
+     <Home /> 
+   {/* <Open /> */}
+   {/* <Body /> */}
+   
       
 
     </div>
