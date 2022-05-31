a110acfce85bdf53448b194c010a00c10ef87926
RellzCodez <adeyemiodunayo21@gmail.com>
2022-05-31 09:01:03 -0700

    modified the project page

--- portfolio/src/Project.js
+++ portfolio/src/Project.js
@@ -1,5 +1,6 @@
 import React from "react";
 import "./Project.css";
+import project1 from './Images/project1.jpg'
 function Project() {
   return (
     <div className="project component__space">
@@ -16,12 +17,64 @@ function Project() {
         <div className="container">
             <div className="row">
                 <div className="col__3">
-                    <div className="project__box pointer">
+                    <div className="project__box pointer relative">
                         <div className="project__box__img pointer relative">
                             <div className="project__img__box">
-                                <img src="" alt="" className="project__img" />
+                                <img src={project1} alt="" className="project__img" />
 
                             </div>
+                            <div className="mask__effect">
+
+                            </div>
+                        </div>
+                        <div className="project__meta absolute">
+                            <h5 className="project__text">Artic Travel</h5>
+                            <h4 className="project__text"> A Travel Agency Website</h4>
+                            <a href="#" className="project__btn"> View Details</a>
+
+                        </div>
+
+                    </div>
+                </div>
+
+                <div className="col__3">
+                    <div className="project__box pointer relative">
+                        <div className="project__box__img pointer relative">
+                            <div className="project__img__box">
+                                <img src={project1} alt="" className="project__img" />
+
+                            </div>
+                            <div className="mask__effect">
+
+                            </div>
+                        </div>
+                        <div className="project__meta absolute">
+                            <h5 className="project__text">Artic Travel</h5>
+                            <h4 className="project__text"> A Travel Agency Website</h4>
+                            <a href="#" className="project__btn"> View Details</a>
+
+                        </div>
+
+                    </div>
+                </div>
+
+
+                <div className="col__3">
+                    <div className="project__box pointer relative">
+                        <div className="project__box__img pointer relative">
+                            <div className="project__img__box">
+                                <img src={project1} alt="" className="project__img" />
+
+                            </div>
+                            <div className="mask__effect">
+
+                            </div>
+                        </div>
+                        <div className="project__meta absolute">
+                            <h5 className="project__text">Artic Travel</h5>
+                            <h4 className="project__text"> A Travel Agency Website</h4>
+                            <a href="#" className="project__btn"> View Details</a>
+
                         </div>
 
                     </div>
